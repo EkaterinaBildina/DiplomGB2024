da75680a64844a91eccc96ecb4367513d79501e5
EkaterinaBildina <kukresch@gmail.com>
2024-04-18 23:26:29 +0700

    server.port info correction

--- db-patterns/src/main/java/com/diploma/dbpatterns/repository/PatternRepository.java
+++ db-patterns/src/main/java/com/diploma/dbpatterns/repository/PatternRepository.java
@@ -2,6 +2,8 @@ package com.diploma.dbpatterns.repository;
 
 import com.diploma.dbpatterns.model.PatternSew;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PatternRepository extends JpaRepository<PatternSew, Long> {
 }
